ff1ad65b226da1f40f71a87ee4055eb42a2d7568
RUDRA KANIYA <41951699+Rudrakaniya@users.noreply.github.com>
2020-04-16 20:22:13 +0530

    adding the basic layout

---
+++ diningProb.c
@@ -0,0 +1,14 @@
+//break_the_bond
+//jatt_james_bond
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+#definde endl "\n"
+
+int32_t main(){
+
+  /* Code will be here */
+
+return 0;
+}
