89cd734ef3d87daf35ffa3af409e8b334f888870
RUDRA KANIYA <41951699+Rudrakaniya@users.noreply.github.com>
2020-04-17 03:08:37 +0530

    Update fileCharQ4.cpp

--- fileCharQ4.cpp
+++ fileCharQ4.cpp
@@ -8,7 +8,7 @@ int main()
 {
     int len, fd;
     char str[30];
-    fd = open("testFile.txt", O_RDONLY, 0777);
+    fd = open("testFile", O_RDONLY, 0777);
     if (fd == -1)
         perror("Error:");
 
